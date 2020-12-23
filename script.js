96663f091ec26cd6c11345a7933872e64edab8eb
DanielPawelko <daniel.pawelko@ocsbstudent.ca>
2020-12-23 04:39:55 +0000

    linter is annoying

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 let randomNum
-let userNum = 1;
-userNum = parseInt(userNum)
-output = document.getElementById("outputLog")
+const output = document.getElementById('outputLog')
 
 createRandom()
 
-function createRandom() {
-  randomNum = Math.floor((Math.random() * 6) + 1);
+function createRandom () {
+  randomNum = Math.floor((Math.random() * 6) + 1)
   console.log(randomNum)
 }
 
+function checkGuess () {
+  const userNum = document.getElementById('userGuess').value
 
-function checkGuess() {
-  let userNum = document.getElementById("userGuess").value;
-
-  if (userNum == randomNum){
-    console.log("Congrats you guessed correctly")
-    output.innerHTML = "Congrats you guessed correctly"
-    output.style.color = "green";
+  if (userNum == randomNum) {
+    console.log('Congrats you guessed correctly')
+    output.innerHTML = 'Congrats you guessed correctly'
+    output.style.color = 'green'
   } else if (isNaN(userNum)) {
-    console.log("Please input a valid number")
-    output.innerHTML = "Please input a valid number"
-    output.style.color = "red";
+    console.log('Please input a valid number')
+    output.innerHTML = 'Please input a valid number'
+    output.style.color = 'red'
   } else {
-    console.log("sorry try again :(")
-    output.innerHTML = "sorry try again :("
-    output.style.color = "red";
+    console.log('sorry try again :(')
+    output.innerHTML = 'sorry try again :('
+    output.style.color = 'red'
   }
 }
